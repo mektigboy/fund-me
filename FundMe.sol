917264b5d059c531ca1259e720c53556dbfc7223
Ángel Sánchez <mektigboy@protonmail.com>
2022-06-14 15:22:44 -0500

    Update FundMe.sol

--- FundMe.sol
+++ FundMe.sol
@@ -10,9 +10,12 @@ contract FundMe {
         require(msg.value >= minimumUsd, "Did not send enough!"); // 1e18 == 1 * 10 ** 18 = 1000000000000000000
     }
 
-    function getPrice() public {
-        // ABI, address: 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e.
-
+    function getPrice() public view returns (uint256) {
+        // ABI
+        // Address: 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        (,int256 price,,,) = priceFeed.latestRoundData();
+        return uint256(price * 1e10); // 1**10 == 10000000000
     }
 
     function getVersion() public view returns (uint256) {
