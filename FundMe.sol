b1465f3697bc958119b2b651daa2d4a8e52da4b4
Ángel Sánchez <mektigboy@protonmail.com>
2022-06-14 16:12:02 -0500

    Update FundMe.sol

--- FundMe.sol
+++ FundMe.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.14;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
-    uint256 public minimumUsd = 50;
+    uint256 public minimumUsd = 50 * 1e18; // 1 * 10 ** 18
 
     function fund() public payable {
-        require(msg.value >= minimumUsd, "Did not send enough!"); // 1e18 == 1 * 10 ** 18 = 1000000000000000000
+        require(getConvertionRate(msg.value) >= minimumUsd, "Did not send enough!"); // 1e18 == 1 * 10 ** 18 = 1000000000000000000
     }
 
     function getPrice() public view returns (uint256) {
